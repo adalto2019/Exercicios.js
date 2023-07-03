c466792ee19d29e58ffc9d1da131785b72687e6e
AdaltoJr <47403579+adalto2019@users.noreply.github.com>
2023-07-02 22:06:20 -0300

    Upload de exercício: função..

---
+++ Exercício função.js
@@ -0,0 +1,11 @@
+//Crie uma função chamada calcula AreaRetangulo que recebe dois parâmetros: largura e altura. 
+//A função deve calcular a área do retângulo e retornar o resultado.
+function calculaAreaRetangulo(l,a){
+    return  l*a
+}
+console.log("Area do retângulo 1: "+calculaAreaRetangulo(6,9)+" m²")
+console.log("Area do retângulo 2: "+calculaAreaRetangulo(123,78)+" m²")
+
+
+
+
